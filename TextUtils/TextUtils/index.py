9179c91bcab36fb6f5263b795bc9847adc2170bf
Just PY <63871463+Pragnyayadav@users.noreply.github.com>
2021-11-27 17:16:54 +0530

    some more changes
    Website kinda works gg

--- TextUtils/TextUtils/index.py
+++ TextUtils/TextUtils/index.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def results(request):
+    djtext = request.GET.get('text','default')
+    djtext = djtext.upper()
+    nfa = {'text':djtext}
+    return render(request, 'results.html', nfa)
